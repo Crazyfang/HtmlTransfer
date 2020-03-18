0df62f368f04433eaa0ff1d05f650f7627553ac4
方勇 <fangyonglubu@outlook.com>
2020-03-18 21:28:25 +0800

    add the annotation

--- Transfer.py
+++ Transfer.py
@@ -2,21 +2,37 @@ from bs4 import BeautifulSoup
 import re
 
 
+# TODO(fangyonglubu@qq.com):Get data from list_string and transform them into data class,
+#  finally insert into table of database
 class TransferClass:
+    """
+    the class use to get base html string from file and then transform them
+    Attributes:None
+    """
     url = 'C:\\Users\\Administrator\\Desktop\\920103_sav_full.htm'
 
     def read_data(self):
+        """
+        Read data from html file and print them, just the function demo and verify the technical feasibility
+        Args:None
+        Returns:None
+        """
+        # open file and read data
         html_file = open(self.url, 'r')
         html_handler = html_file.read()
 
+        # get the bs handler
+        # the features can change to other resolver including html.parser, lxml, xml or lxml-xml, html5lib
         soup = BeautifulSoup(html_handler, features='lxml')
         list_string = []
 
+        # get the table node from the all html string
         for index, table in enumerate(soup.find_all('table')):
             if index == 1:
                 for index1, pre in enumerate(table.find_all('pre')):
                     string = pre.string
                     if string:
+                        # wipe out the blank as far as possible
                         list_string.append(' '.join(string.split()))
 
         # pattern = re.compile(r'[0-9]{4}/[0-9]{2}/[0-9]{2}')
